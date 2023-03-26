65d4cc0da4815389a742a1a779f3df2eebbc4a16
Harrisonkamau <kamauharrison87@gmail.com>
2023-03-26 12:57:16 +0300

    Add more methods to listingsService

--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { listingsService } = require('../services');
 const { handleJoiValidationError } = require('../lib');
+const { Listing } = require('../models');
 
 const router = express.Router();
 
@@ -8,18 +9,8 @@ const router = express.Router();
 router.post('/', async (req, res) => {
   try {
     const { Listing } = req.models;
-    const newListing = await listingsService.createListing(req.body);
-    const { address, price, bedrooms, bathrooms, size, userId } = newListing.value;
-    const listing = new Listing({
-      address,
-      price,
-      bedrooms,
-      bathrooms,
-      size,
-      postedBy: userId,
-    });
-    await listing.save();
-    res.status(201).send(listing);
+    const newListing = await listingsService.createListing({ model: Listing, payload: req.body });
+    res.status(201).send(newListing);
   } catch (error) {
     const { statusCode, errorMessage } = handleJoiValidationError(error);
     res.status(statusCode).send({ errorMessage, statusCode });
@@ -29,65 +20,28 @@ router.post('/', async (req, res) => {
 // Get all listings
 router.get('/', async (req, res) => {
   try {
-    const listings = await Listing.find();
-    console.log({ listings });
+    const listings = await listingsService.getAllListings(Listing);
     res.send(listings);
   } catch (error) {
-    console.log(error);
-    res.status(500).send('Error getting listings');
+    res.status(422).send(error.message);
   }
 });
 
 // Get a specific listing by ID
-router.get('/listings/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
-    const listing = await Listing.findById(req.params.id);
+    const listing = await listingsService.retrieveListingById({
+      model: Listing,
+      listingId: req.params.id,
+    });
+    
     if (!listing) {
-      return res.status(404).send('Listing not found');
+      return res.status(404).send(`No listing with id ${req.params.id} found`);
     }
     res.send(listing);
   } catch (error) {
-    console.log(error);
-    res.status(500).send('Error getting listing');
+    res.status(500).send(error.message);
   }
 });
-
-// // Create a new user
-// router.post('/users', async (req, res) => {
-//   try {
-//     const { name, email, password } = req.body;
-//     const user = new User({ name, email, password });
-//     await user.save();
-//     res.status(201).send(user);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send('Error creating user');
-//   }
-// });
-
-// // Get all users
-// router.get('/users', async (req, res) => {
-//   try {
-//     const users = await User.find();
-//     res.send(users);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send('Error getting users');
-//   }
-// });
-
-// // Get a specific user by ID
-// router.get('/users/:id', async (req, res) => {
-//   try {
-//     const user = await User.findById(req.params.id);
-//     if (!user) {
-//       return res.status(404).send('User not found');
-//     }
-//     res.send(user);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send('Error getting user');
-//   }
-// });
 
 module.exports = router;
--- backend/services/listingsService.js
+++ backend/services/listingsService.js
@@ -13,17 +13,55 @@ module.exports = {
    * @param {Object} payload 
    * @returns {Object | Error} Either returns a validated object or an error
    */
-  async createListing(payload) {
+  async createListing({ payload, model }) {
     try {
       const { value, error } = listingJoiSchema.validate(payload);
       if (error) {
         throw new SchemaValidationError(error.message);
       }
 
-      return value;
-      
+      const { address, price, bedrooms, bathrooms, size, userId } = value;
+      const listing = new model({
+        address,
+        price,
+        bedrooms,
+        bathrooms,
+        size,
+        postedBy: userId,
+      });
+      const newListing = await listing.save();
+      return newListing;
     } catch (error) {
       throw new SchemaValidationError(error.message);
+    }
+  },
+
+  /**
+   * Retrieves a Listing from DB
+   * @param {Object} Mongoose Model
+   * @param {String} listingId mongoDb Id
+   * @returns {Object | Error} MongoDB record found or throws an error
+   */
+  async retrieveListingById({ model, listingId }) {
+    try {
+      const foundItem = await model.findById(listingId);
+      return foundItem;
+    } catch (error) {
+     // @TODO - Improve this error message
+      throw new Error(error.message);
+    }
+  },
+
+  /**
+   * Retrieves all Listings from the DB.
+   */
+  async getAllListings(model) {
+    try {
+      const items = await model.find();
+      return items;
+    } catch (error) {
+      // @TODO - Improve this error message
+     throw new Error(error.message)
     }
   },
 };
